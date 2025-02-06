ec14e020a8e53f6bb925551511a3a3f8b8588cf1
RJA100374 <rjalonso1003@gmail.com>
2025-02-06 14:18:44 +0000

    Added home page

--- flutter_ble/lib/home_page.dart
+++ flutter_ble/lib/home_page.dart
@@ -6,29 +6,78 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // Optional: Remove or customize the AppBar as desired.
       appBar: AppBar(
-        title: const Text(''),
+        title: const Text(
+          'GYM HELPER',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+            letterSpacing: 1.5,
+          ),
+        ),
+        centerTitle: true,
+        elevation: 4,
+        shadowColor: Colors.black,
+        backgroundColor: Colors.white,
       ),
-      body: Center(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.white, Colors.grey[350]!],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Welcome to the Gym App!',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(
+                    Icons.fitness_center_rounded,
+                    size: 220,
+                    color: Colors.black,
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Welcome Back!',
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
               ),
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // TODO: navigate to exercies.dart
-                //
-              },
-              //todo: add style to the elvated button + add an ICON
-              child: const Text('Exercises'),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    //todo: Here toby and Jun part
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    elevation: 4, 
+                    shadowColor: Colors.grey[700],
+                    overlayColor: Colors.grey[800],
+                  ),
+                  child: const Text(
+                    'Continue',
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
