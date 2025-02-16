860e5ee1474269820f5ad9cd96c584c4cad0673c
jun000716 <pokl417716@gmail.com>
2025-02-16 01:43:22 +0000

    dark mode

--- flutter_ble/lib/NavToolBar/Items/settings.dart
+++ flutter_ble/lib/NavToolBar/Items/settings.dart
@@ -1,11 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'theme_provider.dart';
 
 class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
     return Scaffold(
       appBar: AppBar(title: Text("Settings")),
-      body: Center(child: Text("Settings Page", style: TextStyle(fontSize: 24))),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ListTile(
+            leading: Icon(Icons.dark_mode),
+            title: Text("Dark Mode"),
+            trailing: Switch(
+              value: themeProvider.isDarkMode,
+              onChanged: (value) {
+                themeProvider.toggleTheme();
+              },
+            ),
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.person, color: Colors.blue),
+            title: Text("Account"),
+            subtitle: Text("Name: jun\nEmail: xxxxxx@student.bham.ac.uk"),
+            onTap: () {
+              // 계정 설정 화면으로 이동하는 코드 추가 가능
+            },
+          ),
+
+          Divider(),
+
+          ListTile(
+            leading: Icon(Icons.info, color: Colors.green),
+            title: Text("App version"),
+            subtitle: Text("v.1.0.0"),
+          ),
+        ],
+      ),
     );
   }
 }
--- flutter_ble/lib/main.dart
+++ flutter_ble/lib/main.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'home_page.dart';
+import 'NavToolBar/Items/settings.dart';
+import 'NavToolBar/Items/theme_provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: themeProvider.isDarkMode ? ThemeMode.dark : ThemeMode.light,
       home: HomePage(),
     );
   }
